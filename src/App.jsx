d5f9d1967612d16cd96234f6867b2e844ebcba97
hariharan-dev-05 <hariharandeveloper05@gmail.com>
2025-02-11 18:13:27 +0530

    Initiated Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Specializations from "./sections/Specializations";
-import Projects from "./sections/Projects";
 import Skills from "./sections/Skills";
+import Projects from "./sections/Projects";
 import Experience from "./sections/Experience";
-import Testimonials from "./sections/Testimonials";
 import Blog from "./sections/Blog";
-import Contact from "./sections/Contact";
+import Testimonials from "./sections/Testimonials";
 import Companies from "./sections/companies";
+import Contact from "./sections/Contact";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
